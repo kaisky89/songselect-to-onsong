5db811a3763d6bafbfba89921d50eee3b96b09b4
Kai Wittmann <kaisky89@gmail.com>
2023-07-28 17:56:50 +0200

    feat: align to new songselect design

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,124 @@
-/* global $ */
+const $ = <TMultiple extends boolean = false>(
+  selector: string,
+  { multiple }: { multiple?: TMultiple } = {}
+): TMultiple extends true
+  ? NodeListOf<Element> | null | undefined
+  : Element | null | undefined => {
+  const shadowRoot = document.querySelector('#ChordSheetViewerContainer')
+    ?.shadowRoot
+  if (!shadowRoot) return
+  if (!multiple) return shadowRoot.querySelector(selector) as any
+  return shadowRoot.querySelectorAll(selector) as any
+}
 
-// make sure authors are in 1 line
-document
-  .querySelectorAll('.cproAuthors br')
-  .forEach((el) => el.replaceWith(' | '));
+const replace = (selector: string, from: string, to: string) => {
+  const selectedElement = $(selector)
+  if (!selectedElement) return
 
-// handle explicit second author line
-$('.cproAuthor2').prepend('{subtitle: ');
-$('.cproAuthor2').append('}');
+  selectedElement.outerHTML = selectedElement.outerHTML.replace(from, to)
+}
 
-// replace helper
-const replace = (selector, from, to) => {
-  $(selector).html($(selector).html().replace(from, to));
-};
+const download = (filename: string, text: string) => {
+  const element = document.createElement('a')
+  element.setAttribute(
+    'href',
+    `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`
+  )
+  element.setAttribute('download', filename)
+  element.style.display = 'none'
+  document.body.appendChild(element)
+  element.click()
+  document.body.removeChild(element)
+}
 
-// translate
-replace('.cproSongKeyWrapper', 'Tonart', 'Key');
-replace('.cproTempoTimeWrapper', 'Taktart', 'Time');
+const backupHtml = $('div')?.innerHTML
 
-// create syntax
-$('.chord').prepend('[');
-$('.chord').append(']');
-$('.cproComment').append(':');
+////////////////////
+// Header Section //
+////////////////////
 
-// order items
-$('.cproSongKeyWrapper').insertAfter($('.cproAuthors'));
-$('.songnumber').insertAfter($('.cproTempoTimeWrapper'));
+// Songselect Logo
+$('.cproSSLogo')?.remove()
 
-// add line breaks
-$('.cproTitle').append('<br>');
-$('.cproAuthors').append('<br>');
-$('.cproSongKeyWrapper').append('<br>');
+// Authors
+$('.cproAuthors br', { multiple: true })?.forEach((el) => el.replaceWith(' | '))
+$('.cproAuthor2', { multiple: true })?.forEach((el) => {
+  el.prepend('{subtitle: ')
+  el.append('}')
+})
 
-// correct formating
-replace('.cproSongKeyWrapper', ' - ', ': ');
-replace('.cproSongKeyWrapper', ' | ', '');
-replace('.cproTempoTimeWrapper', ' - ', ': ');
-replace('.cproTempoTimeWrapper', ' - ', ': ');
-replace('.cproTempoTimeWrapper', ' | ', '<br>');
-replace('.songnumber', '-Liednummer ', ': ');
+// Key
+replace('.cproSongKeyWrapper', 'Tonart', 'Key')
+replace('.cproSongKeyWrapper', ' - ', ': ')
+replace('.cproSongKeyWrapper', ' | ', '')
+$('.cproSongKeyWrapper')?.append(document.createElement('br'))
 
-// remove songselect logo
-$('.cproSSLogo').remove();
+const tempoTimeWrapper = $('.cproTempoTimeWrapper')
+if (tempoTimeWrapper) {
+  // Tempo
+  const [tempoString, timeString] =
+    $('.cproTempoTimeWrapper')
+      ?.textContent?.split('|')
+      .map((line) => line.trim()) || []
+  tempoTimeWrapper.innerHTML = ''
+  const tempoValue = tempoString?.split('-')[1].trim()
+  const tempoElement = document.createElement('p')
+  tempoElement.innerHTML = `Tempo: ${tempoValue}<br />`
+  tempoElement.classList.add('onsongTempo')
+  $('.cproTempoTimeWrapper')?.append(tempoElement)
+
+  // Time
+  $('.cproSongHeader')?.append(document.createElement('br'))
+  const timeValue = timeString?.split('-')[1].trim()
+  const timeElement = document.createElement('p')
+  timeElement.innerHTML = `Time: ${timeValue}<br />`
+  timeElement.classList.add('onsongTime')
+  $('.cproSongHeader')?.append(timeElement)
+
+  // Song Number
+  tempoTimeWrapper.parentNode?.append($('.songnumber') || '')
+  replace('.songnumber', ' Song #', ':')
+}
+
+////////////////////
+//  Body Section  //
+////////////////////
+
+// Chords
+$('.chord', { multiple: true })?.forEach((el) => {
+  el.prepend('[')
+  el.append(']')
+})
+
+// Section Headings
+$('.cproComment', { multiple: true })?.forEach((el) => el.append(':'))
+
+////////////////////
+// Footer Section //
+////////////////////
+
+// remove copyright footer
+$('.copyright-info')?.remove()
+
+////////////////////
+//   Processing   //
+////////////////////
+
+// get text content
+const headerText = $('.cproSongHeader')
+  ?.textContent?.trim()
+  .split('\n')
+  .filter((line) => !!line.trim())
+  .map((line) => line.trim())
+  .join('\n')
+  .replace(/\s{2}/g, ' ')
+
+const bodyText = $('.cproSongBody')?.textContent?.trim()
+
+const titleText = $('.cproTitle')?.textContent?.trim()
+
+const fullText = `${headerText}\n\n${bodyText}`
+
+download(`${titleText}.onsong`, fullText)
+
+$('div')!.innerHTML = backupHtml || ''
